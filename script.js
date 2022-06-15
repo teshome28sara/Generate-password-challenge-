4a0dec9e13d99071afeea03bbe693c7063ad73f7
sara teshome <tes28sar@gmail.com>
2022-06-15 15:26:16 -0700

    worked on script file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var isAnswer = true;
 
 
 generateBtn.addEventListener("click" , () => {
-  characterLength = parseInt(prompt("how many characters would you like for your password?(8 - 128 "));
+   characterLength = parseInt(prompt("how many characters would you like for your password?(8 - 128 "));
   if(isNaN(characterLength ) || characterLength< 8 || characterLength > 128){
     alert("character  length must be a number, and must be 8 - 128, please try again");
    return false;;
@@ -22,55 +22,52 @@ generateBtn.addEventListener("click" , () => {
       answer2 && answer3 !== isAnswer){
         alert(" must choose at least one character set ");
      return false;
-    }else{
-      return true;
+     }else{
+      return true;;
+     }
+    });
+
+
+      var specialCharacterArr ="!,@,#,$,%,^,&,*,(,)";
+  var lowerCaseArr = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,r,s,t,u,v,w,x,y,z";
+  var upperCaseArr = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,YZ";
+  var numbersArr =  "1234567890";
+
+  function generatePassword(){
+    var options ="".concat(specialCharacterArr,upperCaseArr,numbersArr,lowerCaseArr);
+    
+    var passwordLength = characterLength;
+    var newPassword = "";
+    for(var i = 0; i < passwordLength; i++){
+            options = (Math.floor(Math.random()*options.length));
+            newPassword += options;
+           }
+         return newPassword;
+        }
       
+ 
+
+     function writePassword(){
+              var password = generatePassword();
+               var passwordText = document.querySelector("#password");
+        
+            passwordText.value = password
+               generatePassword();
+               passwordText.value = newPassword;
+        
+             }
+
+  
+
+         
+
+  
+
      
-  }     
+     
+    
     
 
 
-});
 
 
-var specialCharacterArr = ["!", "@", "#", "$", "%", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"];
- var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
- var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
- var numbersArr =  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
- 
-
-// declare an empty array
-function generatePassword(){
- var characters = "";
-  var choiceArr = [];
-  if(specialCharacterArr){
-    choiceArr = choiceArr.concat(specialCharacterArr);
-
-  }if(lowerCaseArr){
-    choiceArr = choiceArr.concat(lowerCaseArr);
-  }if(upperCaseArr){
-    choiceArr = choiceArr.concat(upperCaseArr);
-  }if(numbersArr){
-    choiceArr = choiceArr.concat(numbersArr);
-  }
-  for(var i = 0; i < characters; i++){
-    characters = Math.floor(Math.random()* choiceArr.length)
-  choiceArr += characters;
-  
-
-  }
-  // // Write password to the #password input
- function writePassword() {
-  var passwordText = document.querySelector("#password");
-  var password = generatePassword();
-  
-
-  passwordText.value = password;
-
-
-}
-writePassword();
-
-
-
-}
